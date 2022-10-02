1cd00e6d42e51fbb40a19dd08c354cefbe6114f7
lou <dustyfingers01257@gmail.com>
2022-10-02 16:29:30 -0600

    add termion crate + break clause

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::io::{self, Read};
 
 fn main() {
-
     // read keypresses from user
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
         println!("{}", c);
+        // break from execution when q is pressed
+        if c == 'q' {
+            break;
+        }
     }
 }
