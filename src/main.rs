f1de6bc677820ae8a04604070e63646b4a506419
Louie <dustyfingers01257@gmail.com>
2022-10-03 03:29:16 +0000

    add new comment

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::{self, Read};
 
 fn main() {
     // read keypresses from user
+    // heyooo
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
         println!("{}", c);
