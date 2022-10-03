c8adbbbfb35052fe2909406a8409f56e1c5bd4ce
Louie <dustyfingers01257@gmail.com>
2022-10-03 03:41:44 +0000

    learn more about terminal input modes and improve format of output as user types

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    // set terminal into 'raw' mode
+    // instead of waiting for the enter key is pressed to process input
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     // read keypresses from user
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
+        // b is the byte code of the character pressed!
+        let b = b.unwrap();
+        let c = b as char;
+
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+
         // break from execution when q is pressed
         if c == 'q' {
             break;
