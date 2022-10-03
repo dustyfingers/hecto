47b5b4719e0267fcc46c2f5a220d2c0630fde06f
Louie <dustyfingers01257@gmail.com>
2022-10-03 03:47:42 +0000

    implement control + q keyboard shortcut

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::io::{self, stdout, Read};
 use termion::raw::IntoRawMode;
 
+fn  to_ctrl_byte(c: char) -> u8 {
+    let byte  = c as u8;
+    // bitwise 'magic'
+    byte & 0b0001_1111
+}
+
+fn die(e : std::io::Error) {
+    panic!(e);
+}
+
 fn main() {
     // set terminal into 'raw' mode
     // instead of waiting for the enter key is pressed to process input
@@ -19,7 +29,7 @@ fn main() {
         }
 
         // break from execution when q is pressed
-        if c == 'q' {
+        if b == to_ctrl_byte('q') {
             break;
         }
     }
